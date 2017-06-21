34adbf94d6d8e45602f4558674bf89bd2c4679bd
bernix01 <gbernal096@gmail.com>
2017-06-21 03:55:23 -0500

    bootstrap added, updated webpack and deps versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,101 @@
-var webpack = require('webpack')
-
+const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  entry: "./src/scripts",
+  context: path.resolve(__dirname, 'src'),
+  entry: "./scripts",
   output: {
-    publicPath: "/dist/scripts/",
-    path: __dirname + "/dist/scripts",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, "docs"),
+    filename: '[name].[hash].min.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loader: "babel",
+        loader: "babel-loader",
         exclude: "/node_modules",
-        query: {
+        options: {
           presets: ["es2015", "react"]
         }
-      },
-      {
+      }, {
         test: /\.scss$/,
-        loaders: ["style", "css", "sass"],
-        exclude: "/node_modules"
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //resolve-url-loader may be chained before sass-loader if necessary
+          use: ['css-loader', 'sass-loader']
+        })
+      }, {
+        // ASSET LOADER Reference: https://github.com/webpack/file-loader Copy png, jpg,
+        // jpeg, gif, svg, woff, woff2, ttf, eot files to output Rename the file using
+        // the asset hash Pass along the updated reference to your code You can add here
+        // any file extension you want to get copied to your output
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)([\?]?.*)$/,
+        loader: 'file-loader'
       }
     ]
-  }
+  },
+  resolve: {
+    // options for resolving module requests (does not apply to resolving to
+    // loaders)
+
+    modules: [
+      "node_modules", path.resolve(__dirname, "src")
+    ],
+    // directories where to look for modules
+
+    extensions: [".js", ".json", ".jsx", ".css", ".scss"]
+  },
+  performance: {
+    hints: "warning", // enum
+    maxAssetSize: 200000, // int (in bytes),
+    maxEntrypointSize: 400000, // int (in bytes)
+    assetFilter: function (assetFilename) {
+      // Function predicate that provides asset filenames
+      return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+    }
+  },
+  devServer: {
+    proxy: { // proxy URLs to backend development server
+      //'/api': 'http://localhost:3000'
+    },
+    open: true,
+    contentBase: path.join(__dirname, 'docs'), // boolean | string | array, static file location
+    compress: true, // enable gzip compression
+    historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+    hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+    https: false, // true for self-signed, object for cert authority
+    noInfo: true, // only errors & warns on hot reload
+    // ...
+  },
+
+  plugins: [
+    // build optimization plugins
+    new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
+    new webpack
+      .optimize
+      .CommonsChunkPlugin({name: 'vendor', filename: 'vendor-[hash].min.js'}),
+    new webpack
+      .optimize
+      .UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          drop_console: false
+        }
+      }),
+    new ExtractTextPlugin({filename: 'app-[hash].min.css', allChunks: true}),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // compile time plugins
+    new CleanWebpackPlugin(['docs']),
+    new HtmlWebpackPlugin({template: 'index.html', inject: 'body'}),
+    // webpack-dev-server enhancement plugins
+    new DashboardPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  // list of additional plugins
+
+  watch: true, // boolean
+  // enables watching
 }
