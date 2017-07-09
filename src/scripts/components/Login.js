7617b4aa417b11c7b366ee76d71de284e1a3c7a7
José Luis Massón <jlmasson@espol.edu.ec>
2017-07-09 13:53:10 -0500

    Breakpoint corregido

--- src/scripts/components/Login.js
+++ src/scripts/components/Login.js
@@ -12,41 +12,40 @@ export default class Login extends React.Component {
     return (
       <div>
         <Header page={this.props.location.pathname}/>
-        <div class="container">
             <div class="row">
-                <div class="Absolute-Center is-Responsive">
-                    <div class="col-xl-4 col-xl-offset-4 col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-8 col-xs-offset-2">
-                        <form action="" className="formulario">
+                <div class="container">
+                    <div class="Absolute-Center is-Responsive">
+                        <div class="col-xl-4 col-xl-offset-4 col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-8 col-xs-offset-2">
+                            <form action="" className="formulario">
 
-                            <div id="logo-container"></div>
-                            {/*<img src={"http://via.placeholder.com/300x300"} alt="Avatar" class="avatar"/>*/}
+                                <div id="logo-container"></div>
+                                {/*<img src={"http://via.placeholder.com/300x300"} alt="Avatar" class="avatar"/>*/}
 
-                            <div class="form-group input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                <input class="form-control" type="text" name='username' placeholder="Usuario" required/>
-                            </div>
-                            <div class="form-group input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                <input class="form-control" type="password" placeholder="Contraseña" name="password" required/>
-                            </div>
+                                <div class="form-group input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                    <input class="form-control" type="text" name='username' placeholder="Usuario" required/>
+                                </div>
+                                <div class="form-group input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                    <input class="form-control" type="password" placeholder="Contraseña" name="password" required/>
+                                </div>
 
-                            <div class="checkbox">
-                                <label>
-                                <input type="checkbox"/> Recordarme
-                                </label>
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-success btn-block">Login</button>
-                            </div>
-                            <div class="form-group text-center">
-                                <a href="#">¿Ha olvidado su contraseña?</a>
-                            </div>
-                        </form>
+                                <div class="checkbox">
+                                    <label>
+                                    <input type="checkbox"/> Recordarme
+                                    </label>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-success btn-block">Login</button>
+                                </div>
+                                <div class="form-group text-center">
+                                    <a href="#">¿Ha olvidado su contraseña?</a>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
+                </div>    
             </div>
-            
-        </div>
         <Footer/>
       </div>
     )
