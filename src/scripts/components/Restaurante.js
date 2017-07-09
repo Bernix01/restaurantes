50a307b2e2f7c161c52cd2d6af674edf27c22795
José Luis Massón <jlmasson@espol.edu.ec>
2017-07-09 16:38:11 -0500

    JSON Restaurantes - Armado 20%

--- src/scripts/components/Restaurante.js
+++ src/scripts/components/Restaurante.js
@@ -2,175 +2,188 @@ import React from 'react'
 import Header from './Header'
 import TimelineSemana from './TimelineSemana'
 import Footer from './Footer'
+import axios from 'axios';
 
 export default class Restaurante extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      restaurantData: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://raw.githubusercontent.com/Bernix01/restaurantes/master/src/data/restaurantes.json")
+      .then(res => {
+        console.log(data);
+        const restaurantData = res.data
+        this.setState({ restaurantData });
+      });
   }
 
   render() {
     let {facultad, restaurant} = this.props.match.params
-    let restaurantData = {
-      name: restaurant,
-      img: "http://via.placeholder.com/1850x950",
-      status: "Open",
-      lat:"-2.1468555",
-      lng:"-79.9653056",
-      facultad: facultad,
-      dscr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulpu" +
-          "tate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit" +
-          " elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris mo" +
-          "lestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iac" +
-          "ulis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, veh" +
-          "icula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis " +
-          "sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam " +
-          "odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricie" +
-          "s metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, bland" +
-          "it sed diam.",
-      week: {
-        days: [
-          {
-            date: "Lunes",
-            menus: [
-              {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }
-            ]
-          }, {
-            date: "Martes",
-            menus: [
-              {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }
-            ]
-          }, {
-            date: "Miercoles",
-            menus: [
-              {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }
-            ]
-          }, {
-            date: "Jueves",
-            menus: [
-              {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }
-            ]
-          }, {
-            date: "Viernes",
-            menus: [
-              {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }
-            ]
-          }, {
-            date: "Sabado",
-            menus: [
-              {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }, {
-                img: "http://via.placeholder.com/200x130",
-                dscr: "Well prepared food!"
-              }
-            ]
-          }
-        ]
-      }
-    }
+    // let restaurantData = {
+    //   name: restaurant,
+    //   img: "http://via.placeholder.com/1850x950",
+    //   status: "Open",
+    //   lat:"-2.1468555",
+    //   lng:"-79.9653056",
+    //   facultad: facultad,
+    //   dscr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulpu" +
+    //       "tate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit" +
+    //       " elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris mo" +
+    //       "lestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iac" +
+    //       "ulis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, veh" +
+    //       "icula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis " +
+    //       "sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam " +
+    //       "odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricie" +
+    //       "s metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, bland" +
+    //       "it sed diam.",
+    //   week: {
+    //     days: [
+    //       {
+    //         date: "Lunes",
+    //         menus: [
+    //           {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }
+    //         ]
+    //       }, {
+    //         date: "Martes",
+    //         menus: [
+    //           {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }
+    //         ]
+    //       }, {
+    //         date: "Miercoles",
+    //         menus: [
+    //           {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }
+    //         ]
+    //       }, {
+    //         date: "Jueves",
+    //         menus: [
+    //           {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }
+    //         ]
+    //       }, {
+    //         date: "Viernes",
+    //         menus: [
+    //           {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }
+    //         ]
+    //       }, {
+    //         date: "Sabado",
+    //         menus: [
+    //           {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }, {
+    //             img: "http://via.placeholder.com/200x130",
+    //             dscr: "Well prepared food!"
+    //           }
+    //         ]
+    //       }
+    //     ]
+    //   }
+    // }
     console.log(restaurant)
     return (
       <div>
