4b681727da42097685ee6337acaaea8ca54dc5ce
bernix01 <gbernal096@gmail.com>
2017-07-09 17:48:55 -0500

    map

--- src/scripts/components/Facultad.js
+++ src/scripts/components/Facultad.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import {Link} from 'react-router'
 import axios from 'axios';
+import {withGoogleMap, GoogleMap, Marker} from 'react-google-maps'
+import MarkerClusterer from 'react-google-maps/lib/addons/MarkerClusterer'
 
 import Header from './Header'
 import Footer from './Footer'
+
+const Mapa = withGoogleMap(props => (
+  <GoogleMap
+    defaultZoom={3}
+    defaultCenter={{
+    lat: 25.0391667,
+    lng: 121.525
+  }}>
+    <MarkerClusterer averageCenter enableRetinaIcons gridSize={60}>
+      {props
+        .markers
+        .map(marker => (<Marker
+          position={{
+          lat: marker.latitude,
+          lng: marker.longitude
+        }}
+          key={marker.photo_id}/>))}
+    </MarkerClusterer>
+  </GoogleMap>
+));
 
 export default class Facultad extends React.Component {
   constructor(props) {
@@ -12,213 +34,83 @@ export default class Facultad extends React.Component {
       facultadData: []
     };
   }
-  
+
   componentDidMount() {
-    axios.get("https://raw.githubusercontent.com/Bernix01/restaurantes/master/src/data/facultades.json")
+    axios
+      .get("https://raw.githubusercontent.com/Bernix01/restaurantes/master/src/data/facultad" +
+        "es.json")
       .then(res => {
         const facultadData = res.data
-        this.setState({ facultadData });
+        this.setState({facultadData});
       });
   }
-  
+
   render() {
     let facultadData = this.state.facultadData
-    // let facultadData = [
-    //   {
-    //     name: "FCNM",
-    //     dscr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulpu" +
-    //         "tate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit" +
-    //         " elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris mo" +
-    //         "lestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iac" +
-    //         "ulis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, veh" +
-    //         "icula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis " +
-    //         "sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam " +
-    //         "odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricie" +
-    //         "s metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, bland" +
-    //         "it sed diam.",
-    //     restaurants: [
-    //       {
-    //         name: "CELEX",
-    //         status: "Open",
-    //         img: "http://via.placeholder.com/300x300",
-    //         likes: 22,
-    //         liked: true
-    //       }, {
-    //         name: "COCA COLA",
-    //         status: "Open",
-    //         img: "http://via.placeholder.com/300x300",
-    //         likes: 22,
-    //         liked: true
-    //       }, {
-    //         name: "CELEX",
-    //         status: "Open",
-    //         img: "http://via.placeholder.com/300x300",
-    //         likes: 22,
-    //         liked: true
-    //       }
-    //     ]
-    //   }, {
-    //     name: "FCNM",
-    //     dscr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulpu" +
-    //         "tate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit" +
-    //         " elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris mo" +
-    //         "lestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iac" +
-    //         "ulis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, veh" +
-    //         "icula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis " +
-    //         "sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam " +
-    //         "odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricie" +
-    //         "s metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, bland" +
-    //         "it sed diam.",
-    //     restaurants: [
-    //       {
-    //         name: "CELEX",
-    //         status: "Open",
-    //         likes: 22,
-    //         img: "http://via.placeholder.com/300x300",
-    //         liked: true
-    //       }, {
-    //         name: "COCA COla",
-    //         status: "Open",
-    //         likes: 22,
-    //         img: "http://via.placeholder.com/300x300",
-    //         liked: true
-    //       }, {
-    //         name: "CELEX",
-    //         status: "Open",
-    //         likes: 22,
-    //         img: "http://via.placeholder.com/300x300",
-    //         liked: true
-    //       }
-    //     ]
-    //   }, {
-    //     name: "FCNM",
-    //     dscr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulpu" +
-    //         "tate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit" +
-    //         " elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris mo" +
-    //         "lestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iac" +
-    //         "ulis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, veh" +
-    //         "icula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis " +
-    //         "sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam " +
-    //         "odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricie" +
-    //         "s metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, bland" +
-    //         "it sed diam.",
-    //     restaurants: [
-    //       {
-    //         name: "CELEX",
-    //         status: "Open",
-    //         likes: 22,
-    //         img: "http://via.placeholder.com/300x300",
-    //         liked: true
-    //       }, {
-    //         name: "COCA COla",
-    //         status: "Open",
-    //         likes: 22,
-    //         img: "http://via.placeholder.com/300x300",
-    //         liked: true
-    //       }, {
-    //         name: "CELEX",
-    //         status: "Open",
-    //         likes: 22,
-    //         img: "http://via.placeholder.com/300x300",
-    //         liked: true
-    //       }
-    //     ]
-    //   }, {
-    //     name: "FCNM",
-    //     dscr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulpu" +
-    //         "tate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit" +
-    //         " elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris mo" +
-    //         "lestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iac" +
-    //         "ulis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, veh" +
-    //         "icula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis " +
-    //         "sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam " +
-    //         "odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricie" +
-    //         "s metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, bland" +
-    //         "it sed diam.",
-    //     restaurants: [
-    //       {
-    //         name: "CELEX",
-    //         status: "Open",
-    //         img: "http://via.placeholder.com/300x300",
-    //         likes: 22,
-    //         liked: true
-    //       }, {
-    //         name: "COCA COla",
-    //         img: "http://via.placeholder.com/300x300",
-    //         status: "Open",
-    //         likes: 22,
-    //         liked: true
-    //       }, {
-    //         name: "CELEX",
-    //         img: "http://via.placeholder.com/300x300",
-    //         status: "Open",
-    //         likes: 22,
-    //         liked: true
-    //       }
-    //     ]
-    //   }, {
-    //     name: "FCNM",
-    //     dscr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulpu" +
-    //         "tate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit" +
-    //         " elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris mo" +
-    //         "lestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iac" +
-    //         "ulis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, veh" +
-    //         "icula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis " +
-    //         "sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam " +
-    //         "odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricie" +
-    //         "s metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, bland" +
-    //         "it sed diam.",
-    //     restaurants: [
-    //       {
-    //         name: "CELEX",
-    //         status: "Open",
-    //         likes: 22,
-    //         liked: true
-    //       }, {
-    //         name: "COCA COla",
-    //         status: "Open",
-    //         likes: 22,
-    //         liked: true
-    //       }, {
-    //         name: "CELEX",
-    //         status: "Open",
-    //         likes: 22,
-    //         liked: true
-    //       }
-    //     ]
-    //   }
-    // ]
-    return (
-      <div>
-        <Header page={this.props.location.pathname}/>
-        <div className="container-fluid">
-          {facultadData.map((facultad, index) => {
-            let restaurants = facultad
-              .restaurants
-              .map((restaurant, index) => {
-                return (
-                  <div className="col-sm-3">
-                    <a href={`/facultades/${facultad.name}/${restaurant.name}`}>
-                      <img src={restaurant.img} className="img-responsive" alt=""/>
-                      <h4>{restaurant.name}</h4>
-                      <p>{restaurant.status}</p>
-                    </a>
-                  </div>
-                )
-              })
-            return (
-              <div className="row">
-                <div className="col-sm-12">
-                  <h3>{facultad.name}</h3>
-                  <p>{facultad.dscr}</p>
-                  {restaurants}
-                </div>
+    let markers = []
+    if (facultadData.length > 0) {
+
+      markers = facultadData.reduce((prev, curr, i) => {
+        if (i == 1) {
+          let a = []
+          a = a.concat(curr.restaurants)
+          console.log("a", a)
+          return a
+        }
+
+        let b = prev.concat(curr.restaurants)
+        console.log(b)
+        return b
+      })
+    }
+    console.log("markers", markers)
+    let container = <div style={{
+      height: `100%`
+    }}/>
+    let mapElem = <div style={{
+      height: `100%`
+    }}/>
+    if (facultadData) 
+      return (
+        <div>
+          <Header page={this.props.location.pathname}/>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-sm-12">
+                <Mapa containerElement={container} mapElement={mapElem} markers={markers}/>
               </div>
-            )
-          })}
+            </div>
+            {facultadData.map((facultad, index) => {
+              let restaurants = facultad
+                .restaurants
+                .map((restaurant, index) => {
+                  return (
+                    <div className="col-sm-3">
+                      <a href={`/facultades/${facultad.name}/${restaurant.name}`}>
+                        <img src={restaurant.img} className="img-responsive" alt=""/>
+                        <h4>{restaurant.name}</h4>
+                        <p>{restaurant.status}</p>
+                      </a>
+                    </div>
+                  )
+                })
+              return (
+                <div className="row">
+                  <div className="col-sm-12">
+                    <h3>{facultad.name}</h3>
+                    <p>{facultad.dscr}</p>
+                    {restaurants}
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+          <Footer/>
         </div>
-        <Footer/>
-      </div>
+      )
+
+    return (
+      <div>Cargando..</div>
     )
   }
 }
