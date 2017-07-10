f7b94f24c63faf2df30a62194120803c50976294
bernix01 <gbernal096@gmail.com>
2017-07-09 23:37:00 -0500

    added *

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.post('/', function (req, res) {
 app.listen(process.env.PORT || 80, function () {
     console.log('Node app is running on port', process.env.PORT || 80);
 });
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(__dirname + '/docs/index.html');
 });
