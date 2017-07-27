2a6b732f8fd6a445025d50a9bfaf1d89cf23aef0
José Luis Massón <jlmasson@espol.edu.ec>
2017-07-27 11:12:02 -0500

    Listar facturas

--- src/scripts/components/Facturas.js
+++ src/scripts/components/Facturas.js
@@ -8,35 +8,68 @@ import Footer from './Footer'
 export default class Facturas extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      facturaData: []
+    };
   }
 
   componentDidMount() {
     axios
-      .get("https://raw.githubusercontent.com/Bernix01/restaurantes/master/src/data/facultad" +
-        "es.json")
+      .get("https://raw.githubusercontent.com/Bernix01/restaurantes/master/src/data/facturas" +
+        ".json")
       .then(res => {
-        const facultadData = res.data
-        this.setState({facultadData});
+        const facturaData = res.data["facturas"]
+        this.setState({facturaData});
       });
   }
 
   render() {
+
+    let facturaData = this.state.facturaData
+
     return (
       <div>
         <Header page={this.props.location.pathname}/>
             <div class="main container-fluid">    
                 <div class="row">
-                    <div class="col-md-12">
+                    <div class="col-md-8 col-md-offset-2">
+                      <div className="card-header">
+                        <h1>Facturas</h1>
+                      </div>
+                      <div className="card-content table-responsive">
                         <table class="table table-bordered table-hover">
                           <thead>
-                          <tr>
-                            <th>Empresa</th>
-                            <th>Fecha Límite</th>
-                            <th>Total</th>
-                            <th>Estado</th>
-                            <th>Opciones</th>
-                          </tr></thead>
+                            <tr>
+                              <th>Empresa</th>
+                              <th>Fecha Límite</th>
+                              <th>Total</th>
+                              <th>Estado</th>
+                              <th>Opciones</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {facturaData.map((factura, index) => {              
+                              return (
+                                <tr>
+                                  <td>{factura.empresa}</td>
+                                  <td>{factura.fecha}</td>
+                                  <td>{factura.total}</td>
+                                  <td>{factura.estado}</td>
+                                  <td class="td-action">
+                                    <a href="#" rel="tooltip" title="Editar" class="btn btn-simple btn-warning btn-xs" data-original-title="Editar">
+                                      <i class="fa fa-pencil"></i>
+                                    </a>
+                                    <a href="#" rel="tooltip" title="Eliminar" class="btn-simple btn btn-danger btn-xs" data-original-title="Eliminar">
+                                      <i class="fa fa-remove"></i>
+                                    </a>
+                                  </td>
+                                </tr>
+                              )
+                            })
+                          }
+                          </tbody>
                         </table>
+                      </div>
                     </div>    
                 </div>
             </div>
