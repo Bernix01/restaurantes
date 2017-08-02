af9c62bc6b181965594c7e43300d92531367f3b3
José Luis Massón <jlmasson@espol.edu.ec>
2017-08-02 10:29:41 -0500

    Modelo Recibo modificado

--- src/models/ReciboModel.js
+++ src/models/ReciboModel.js
@@ -5,15 +5,15 @@ const ReciboSchema = new Schema({
 	numRecibo: {type: String, required: true, unique: true},
     fechaPago: {type: Date, required: true},
     nombreRecibido: {type: String, required: true},
-    estado: {type: String, required: true},
-	concepto: {type: Number, required: true}
+    concepto: {type: String, required: true},
+	cantidad: {type: Number, required: true},
 })
 
-FacturaSchema.path('numRecibo').validate(function(dato){
+ReciboSchema.path('numRecibo').validate(function(dato){
 	return dato.length === 16;
 });
 
-const Factura = mongoose.model('recibo', ReciboSchema, 'recibo');
+const Recibo = mongoose.model('recibo', ReciboSchema, 'recibo');
 
 module.exports = {
 	Recibo: Recibo
