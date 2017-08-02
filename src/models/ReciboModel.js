336321ca5d6a775dcdd79e5a78846ebc85ae081d
José Luis Massón <jlmasson@espol.edu.ec>
2017-08-02 09:20:02 -0500

    Esquema de Recibo lista

---
+++ src/models/ReciboModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//modelo factura
+const ReciboSchema = new Schema({
+	numRecibo: {type: String, required: true, unique: true},
+    fechaPago: {type: Date, required: true},
+    nombreRecibido: {type: String, required: true},
+    estado: {type: String, required: true},
+	concepto: {type: Number, required: true}
+})
+
+FacturaSchema.path('numRecibo').validate(function(dato){
+	return dato.length === 16;
+});
+
+const Factura = mongoose.model('recibo', ReciboSchema, 'recibo');
+
+module.exports = {
+	Recibo: Recibo
+};
